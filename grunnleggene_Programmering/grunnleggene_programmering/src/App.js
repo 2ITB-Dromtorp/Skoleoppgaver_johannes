bb7d015cca7ef81ce0fb1822b7b6769718bdd1a6
Johannes Herman Rebård Evensen <johannes.evensen@outlook.com>
2023-08-31 09:35:52 +0200

    make it work

---
+++ grunnleggene_Programmering/grunnleggene_programmering/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import './index.js';
+
+
+function A() {
+  return (
+    <div className='oppgavebox'>
+      <h2>dellopgave 1 a)</h2>
+      <p>spørsmål: hva skriver du i Javascrip dersom du vil he variablen test verdi 8? hvilkjen datta type er dette?</p>
+      <p>svar: let test = 8;</p>
+    </div>
+  );
+}
+
+export default function oppgave1() {
+  return (
+    <div>
+      <h1>Oppgave 1</h1>
+      <header className='App-header'></header>
+    </div>
+  );
+}
+
+
