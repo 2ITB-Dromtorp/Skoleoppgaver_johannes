3c45696f1735b1321937109afc5c3352860b9b39
Johannes Herman Rebård Evensen <johannes.evensen@outlook.com>
2023-09-12 14:06:43 +0200

    yeah

--- grunnleggene_Programmering/grunnleggene_programmering/src/oppgave 3.js
+++ grunnleggene_Programmering/grunnleggene_programmering/src/oppgave 3.js
@@ -4,31 +4,32 @@ import React, { useState } from 'react';
 
 function Land() {
 
-  const [inputNorsk, setInputNorsk] = useState('')
-  const [inputSvensk, setInputSvensk] = useState('')
+  const [nationalitet, setNationalitet] = useState('')
+  const [svar, setSvar] = useState('')
 
-  function handleClick() {
-    
-
+  function checkNationality() {
+    if (nationalitet == 'n') {
+      setSvar('norsk')
+    }
+    if (nationalitet == 's') {
+      setSvar('svensk')
+    } 
   }
 
+
+  
   const handleChange = (event) => {
-    setInputNorsk(event.target.value)
+    setNationalitet(event.target.value)
   };
 
-  const handleChange2 = (event) => {
-    setInputSvensk(event.target.value)
-  };
 
   return (
     <div className='oppgavebox4'>
         <h1 className='op31'>oppgave 3</h1>
-        <h1 className='op32'>er du Norsk eller Svensk? (skrive N for nosk eller S for svensk)</h1>
-        <p>Length:</p>
-        <input type="text" value={inputNorsk} onChange={handleChange}/>
-        <p>Width: </p>
-        <input type="text" value={inputSvensk} onChange={handleChange2}/> <br></br>
-        <p>du er: {inputNorsk * inputSvensk} </p>
+        <h1 className='op32'>er du Norsk eller Svensk? (skrive N for nosk eller S for svensk)</h1> <br></br> <br></br>
+        <input type="text" value={nationalitet} onChange={handleChange}/> <br></br> <br></br>
+        <input type="submit" onClick={checkNationality}/> <br></br>
+        <p>Du er: {svar} </p>
       </div>
   )
 }
