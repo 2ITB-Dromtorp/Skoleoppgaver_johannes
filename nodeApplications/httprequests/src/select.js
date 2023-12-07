479a2b01b9a36a545bd1bb6d5d52a76ef71f1a4d
Johannes Herman Rebård Evensen <johannes.evensen@outlook.com>
2023-12-07 15:35:56 +0100

    lezgo

--- nodeApplications/httprequests/src/select.js
+++ nodeApplications/httprequests/src/select.js
@@ -1,20 +1,54 @@
-import { useEffect } from 'react';
-import './content.css';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import './table_style.css';
 
 export default function Select() {
+    const [elevData, setElevData] = useState([]);
 
     useEffect(() => {
+        getElevData();
+    }, []);
 
-    axios
-        .get("http://localhost:3002")
-        .then(data => console.log(data.data))
-        .catch(error => console.log(error));
+    const getElevData = () => {
+        axios
+            .get("http://localhost:3002/")
+            .then(response => {
+                setElevData(response.data);
+            })
+            .catch(error => console.log(error));
+    };
 
-}, []);
     return (
-        <div className="container">
-            Her kommer en tabell med select 
+        <>
+        <div className="table-container">
+            
+            <table className="styled-table">
+                <thead>
+                    <tr>
+                        <th>ElevID</th>
+                        <th>Fornavn</th>
+                        <th>Etternavn</th>
+                        <th>DatamaskinID</th>
+                        <th>Hobby</th>
+                        <th>Klasse</th>
+                        <th>Kjønn</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {elevData.map(elev => (
+                        <tr key={elev.ElevID}>
+                            <td>{elev.ElevID}</td>
+                            <td>{elev.Fornavn}</td>
+                            <td>{elev.Etternavn}</td>
+                            <td>{elev.DatamaskinID}</td>
+                            <td>{elev.Hobby}</td>
+                            <td>{elev.Klasse}</td>
+                            <td>{elev.Kjonn}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
-    )
+        </>
+    );
 }
