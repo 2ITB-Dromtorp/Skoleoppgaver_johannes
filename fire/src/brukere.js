e31c804ebbb5d598e81461609c9871258eb190de
Johannes Herman Rebård Evensen <johannes.evensen@outlook.com>
2024-01-25 15:25:15 +0100

    Fembot Hooters

---
+++ fire/src/brukere.js
@@ -0,0 +1,11 @@
+let config = {
+    "brukere" : [
+        {
+            "navn" : "Astolfo",
+            "email": "BBOP@gmail.com",
+            "passord" : "BBOP",
+        },
+    ]
+}
+
+export default config;
